b5231d997e454bb7022630b5d6e94f7df8f9f41d
frol panasyuk <froli68f2016@yandex.ru>
2024-06-14 22:23:39 +0300

    gitignore

--- src/main.py
+++ src/main.py
@@ -1,16 +1,19 @@
-# This is a sample Python script.
+def mask_card_number(card_number):
+    """Функция для номера карты"""
+    if len(card_number) == 16 and card_number.isdigit(): # Проверяем, что номер карты состоит из 16 цифр
+        return f"{card_number[:4]} {card_number[4:6]}** **** {card_number[-4:]}" # Возвращаем маскированный номер карты
+    else:
+        return "Неверный формат номера карты"
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+def mask_account_number(account_number):
+    """Функция для номера счета"""
+    if len(account_number) >= 4 and account_number.isdigit(): # Проверяем, что номер счета состоит из минимум 4 цифр
+        return f"Счёт: ****{account_number[-4:]}" # Возвращаем маскированный номер счета
+    else:
+        return "Неверный формат номера счета"
 
+card_num = "1234567890123456"
+account_num = "12345678"
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+print(mask_card_number(card_num))  # Вывод: 1234 56** **** 3456
+print(mask_account_number(account_num))  # Вывод: ****5678
