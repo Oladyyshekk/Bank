b8f89c4c0a52d73100fa955739e80f636a68a1a6
Исайченков Дмитрий <spy.teamred@mail.ru>
2024-06-22 17:08:54 +0300

    test1

--- src/main.py
+++ src/main.py
@@ -1,14 +1,15 @@
+import widget
 def mask_card_number(card_number):
     """Функция для номера карты"""
-    if len(card_number) == 16 and card_number.isdigit(): # Проверяем, что номер карты состоит из 16 цифр
-        return f"{card_number[:4]} {card_number[4:6]}** **** {card_number[-4:]}" # Возвращаем маскированный номер карты
+    if len(card_number) == 16 and card_number.isdigit():  # Проверяем, что номер карты состоит из 16 цифр
+        return f"{card_number[:4]} {card_number[4:6]}** **** {card_number[-4:]}"  # Возвращаем маскированный номер карты
     else:
         return "Неверный формат номера карты"
 
 def mask_account_number(account_number):
     """Функция для номера счета"""
-    if len(account_number) >= 4 and account_number.isdigit(): # Проверяем, что номер счета состоит из минимум 4 цифр
-        return f"**{account_number[-4:]}" # Возвращаем маскированный номер счета
+    if len(account_number) >= 4 and account_number.isdigit():  # Проверяем, что номер счета состоит из минимум 4 цифр
+        return f"**{account_number[-4:]}"  # Возвращаем маскированный номер счета
     else:
         return "Неверный формат номера счета"
 
